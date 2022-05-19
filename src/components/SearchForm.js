16ec861f2bacad2d08b9e89d1ae34b9c05ae9a53
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2022-05-18 19:13:43 -0500

    Fix search validation

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,28 +1,33 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import style from './SearchForm.module.css';
+import style from "./SearchForm.module.css";
 
 const SearchForm = () => {
+    const [subject, setSubject] = useState("");
 
-    const [subject, setSubject] = useState('');
-
-    const subjectChangeHandler = event => {
+    const subjectChangeHandler = (event) => {
         setSubject(event.target.value);
-    }
+    };
 
     let history = useHistory();
 
-    const submitHandler = event => {
+    const submitHandler = (event) => {
         event.preventDefault();
-        if(subject){
-            history.push(`/subjects/${subject.toLowerCase().replaceAll(' ', '_')}/1`)
+        if (!subject.trim().length) {
+            console.log("Alert!");
+        } else {
+            history.push(
+                `/subjects/${subject.trim().toLowerCase().replaceAll(" ", "_")}/1`
+            );
         }
-        setSubject('');
-    }
+        setSubject("");
+    };
 
     return (
         <form className={style.form} onSubmit={submitHandler}>
-            <label className={style.label} htmlFor="subject">Subject</label>
+            <label className={style.label} htmlFor="subject">
+                Subject
+            </label>
             <input
                 placeholder="Search books by subject"
                 className={style.input}
@@ -32,9 +37,11 @@ const SearchForm = () => {
                 name="subject"
                 id="subject"
             />
-            <button className={style.btn} type="submit">Search</button>
+            <button className={style.btn} type="submit">
+                Search
+            </button>
         </form>
-    )
-}
+    );
+};
 
 export default SearchForm;
